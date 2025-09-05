c985f7eacc8379fed40e3eb65ad5e19f3cadb9c9
Riccardo <posta.riky@gmail.com>
2025-09-05 17:58:54 +0200

    db

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,10 +1,10 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-  host: process.env.DB_HOST || 'db',         // 'db' = nome servizio docker
+  host: process.env.DB_HOST || 'skytravel-db',         // 'db' = nome servizio docker
   port: parseInt(process.env.DB_PORT || '5432', 10),
   user: process.env.DB_USER || 'admin',
-  password: process.env.DB_PASSWORD || 'your_password',
+  password: process.env.DB_PASSWORD || 'postgers',
   database: process.env.DB_NAME || 'SkyTravelDb',
   max: 10,
   idleTimeoutMillis: 30000,
