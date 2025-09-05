4c77285a736e9b0af63bcecff011b4af7075b480
Franci <wavingchito@gmail.com>
2025-09-05 16:54:00 +0200

    ciao

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,20 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
 const PORT: number = 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN?.split(",") ?? ["http://localhost:4200"],
+  credentials: true
+}));
 
 // Rotta di prova
 app.get("/", (req: Request, res: Response) => {
